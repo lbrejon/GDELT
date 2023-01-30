4cc89a3e123903c43295a91bea11feed7495c2a0
lbrejon <louis.brejon.ia@gmail.com>
2023-01-30 10:05:36 +0100

    update scripts for uploading data

--- src/functions/deploy.sh
+++ src/functions/deploy.sh
@@ -1,63 +1,68 @@
+#!/bin/bash
+#!/bin/sh
+#include <libgen.h>
+
+
 TIME=3
 
-# Specify user parameters
+
+# Specify parameters
 login="lbrejon-21"
 remoteFolderLogin="/tmp/$login/" 
 bridge="$login@ssh.enst.fr"
 bridge_ubuntu="ubuntu@137.194.211.146"
+github_repository="https://github.com/lbrejon/GDELT.git"
 
 # Program to deploy on remote computers
 flag_files=true
-# files=('client.py' 'utils.py' '../config/params.yaml' '../data/wet.paths.gz')
-files=('test_deploy.py')
-
-for file in ${files[@]};do
-    if [ ! -f "$file" ];then
-        echo "ERROR: '$file' file does not exist."
-        flag_files = false
-    else
-        echo "'$file' file exists."
-    fi
-done
-echo
-
+# files=("server.py" "utils.py" "../config/params.yaml")
+# for file in ${files[@]};do
+#     if [ ! -f "$file" ];then
+#         echo "ERROR: '$file' file does not exist."
+#         flag_files = false
+#     else
+#         echo "'$file' file exists."
+#     fi
+# done
+# echo
 
 if $flag_files; then
-    
     # Specify computers to use 
-    computers=("ubuntu@tp-hadoop-60")
+    computers=("tp-hadoop-43" "tp-hadoop-54")
+    # computers=("tp-hadoop-43.enst.fr" "tp-hadoop-54.enst.fr" "tp-hadoop-45.enst.fr" "tp-hadoop-30.enst.fr")
+
     echo "Ready to execute '${files[0]}' file on remote computers: ${computers}."
 
     # Run commands
-    for computer in ${computers[@]}; do
-
-        # command0=("ssh" "-t" "$bridge" "ssh" "-t" "$bridge_ubuntu" "ssh" "-t" "$computer" "lsof -ti | xargs kill -9") # listing and killing process
-       
-
-        # command1=("ssh" "-t" "$bridge" "ssh" "-t" "$bridge_ubuntu" "ssh" "-t" "$computer" "echo 'Connected!' && rm -rf $remoteFolderLogin && echo 'Still connected?'")
-        command1=("ssh" "-t" "$bridge" "ssh" "-t" "$bridge_ubuntu" "ssh" "-t" "$computer" "rm -rf $remoteFolderLogin")
-        command2=("ssh" "-t" "$bridge" "ssh" "-t" "$bridge_ubuntu" "ssh" "-t" "$computer" "mkdir -p $remoteFolderLogin")
-        command3=("scp" "-J" "$bridge" "$bridge_ubuntu" "'${files[0]}'" "$computer:'$remoteFolderLogin${files[0]}'")
-        #command3=("ssh" "-t" "ubuntu@137.194.211.146" "ssh" "-t" "$c" "cd $remoteFolderLogin;python ${files[0]}$ext")
-        # command21=("scp" "-J" "$bridge" "${files[0]}" "$login@$c:$remoteFolderLogin${files[0]}") # copy server.py
-
-
-
-        # echo ${command0[*]}
-        # "${command0[@]}"
-        # echo
-
+    for c in ${computers[@]}; do
+        # command0=("ssh" "-t" "$bridge" "ssh" "-t" "$bridge_ubuntu" "ssh" "-t" "$c" "lsof -ti | xargs kill -9") # listing and killing process
+        command1=("ssh" "-t" "$bridge" "ssh" "-t" "$bridge_ubuntu" "ssh" "-t" "$c" "rm -rf $remoteFolderLogin;mkdir $remoteFolderLogin")
+        command2=("ssh" "-t" "$bridge" "ssh" "-t" "$bridge_ubuntu" "ssh" "-t" "$c" "cd $remoteFolderLogin;git clone ${github_repository}") # copy server.py
+        # command2=("ssh" "-J" "$bridge" "$bridge_ubuntu" "$login@$c" "cd $remoteFolderLogin;python3 ${files[0]}") # run server.py script
+        
+        echo
+        echo "CLEANING..."
+        echo ${command0[*]}
+        "${command0[@]}"
         echo ${command1[*]}
         "${command1[@]}"
-        echo
 
-        echo ${command2[*]}
-        "${command2[@]}"
-        echo
 
-        echo ${command3[*]}
-        "${command3[@]}"
         echo
+        # echo "CLONING REPOSITORY..."
+        # echo ${command2[*]}
+        # "${command2[@]}"
+        # sleep $TIME
+        # echo ${command22[*]}
+        # "${command22[@]}"
+        # sleep $TIME
 
+
+        # echo
+        # echo "UPLOAD DATA..."
+        # echo ${command2[*]}
+        # "${command2[@]}" &
     done
 fi
+
+echo ">>>>>>>>>> END ./deploy_server.sh <<<<<<<<<<"
